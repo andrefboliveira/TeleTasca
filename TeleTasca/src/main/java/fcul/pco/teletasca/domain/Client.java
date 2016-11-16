6c2f0e8be9d763ddd4dd4520754c9912ef404266
André Oliveira <andrefboliveira@users.noreply.github.com>
2016-11-16 13:18:56 +0000

    Adicionado hashCode e equals. Orders a funcionar completamente

--- TeleTasca/src/main/java/fcul/pco/teletasca/domain/Client.java
+++ TeleTasca/src/main/java/fcul/pco/teletasca/domain/Client.java
@@ -82,4 +82,31 @@ public class Client {
 		return new Client(clientName, clientEmail);
 
 	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 17;
+		result = prime * result + email.hashCode();
+		return result;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof Client))
+			return false;
+		Client other = (Client) obj;
+		return this.email.equals(other.getEmail());
+	
+	}
 }
