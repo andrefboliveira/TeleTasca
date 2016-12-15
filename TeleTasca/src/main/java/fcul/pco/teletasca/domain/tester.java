b0afc7c68cf32456ea5b77069f6845f89c632460
tanmald <tanmald@hotmail.com>
2016-12-15 18:30:52 +0000

    tester

--- TeleTasca/src/main/java/fcul/pco/teletasca/domain/tester.java
+++ TeleTasca/src/main/java/fcul/pco/teletasca/domain/tester.java
@@ -1,14 +1,16 @@
 package fcul.pco.teletasca.domain;
 //import fcul.pco.teletasca.domain.NutritionFacts;
 import fcul.pco.teletasca.domain.Dish;
+import fcul.pco.teletasca.domain.DishCatalog;
 
 public class tester {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 //		NutritionFacts nut = new NutritionFacts(1, 1, 500, 5, 0.1, 46.7);
-//		Dish pratoteste = new Dish("arroz de pato", 5, 500, 1, 750, 8, 0.5, 89);
-		System.out.println(Dish.fromString("arroz de pato, 5, 500, 1, 750, 8, 0.5, 89"));
+		fcul.pco.teletasca.main.App.dishCatalog = DishCatalog.getDishCatalog();
+		Dish pratoteste = new Dish("arroz de pato", 5, 500, 1, 750, 8, 0.5, 89);
+		System.out.println(pratoteste);
 	}
 
 }
