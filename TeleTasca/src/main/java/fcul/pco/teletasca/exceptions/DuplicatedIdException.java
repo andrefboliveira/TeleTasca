64eba5e2c191e44ed65c73da92b5e6f5f4cbc07f
André Oliveira <andrefboliveira@users.noreply.github.com>
2016-12-19 18:43:03 +0000

    Correção de Revert

---
+++ TeleTasca/src/main/java/fcul/pco/teletasca/exceptions/DuplicatedIdException.java
@@ -0,0 +1,8 @@
+package fcul.pco.teletasca.exceptions;
+
+public class DuplicatedIdException extends InvalidIdException {
+	
+	public DuplicatedIdException (String message) {
+		super(message);
+	}
+}
---
+++ TeleTasca/src/main/java/fcul/pco/teletasca/exceptions/InvalidDateException.java
@@ -0,0 +1,8 @@
+package fcul.pco.teletasca.exceptions;
+
+public class InvalidDateException extends BadDataFormatException {
+	
+	public InvalidDateException (String message){
+		super(message);
+	}
+}
