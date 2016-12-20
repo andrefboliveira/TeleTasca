a56052036148de422e90214dad05033f8348a0cf
tanmald <tanmald@hotmail.com>
2016-12-20 12:54:56 +0000

    adicionados contratos do pacote exceptions

--- TeleTasca/src/main/java/fcul/pco/teletasca/exceptions/InvalidDateException.java
+++ TeleTasca/src/main/java/fcul/pco/teletasca/exceptions/InvalidDateException.java
@@ -1,7 +1,22 @@
 package fcul.pco.teletasca.exceptions;
 
+/**
+ * The Duplicated Id Exception class represents an exception to be 
+ * launched if an invalid date is found within a catalog.
+ *
+ * @author Thibault Langlois
+ * Alunos:
+ * @author André Oliveira 45648
+ * @author Tânia Maldonado 44745
+ *
+ */
 public class InvalidDateException extends BadDataFormatException {
 	
+	/**
+	 * A method to launch a defined message for the exception to be launched.
+	 * 
+	 * @param message, a string parameter
+	 */
 	public InvalidDateException (String message){
 		super(message);
 	}
